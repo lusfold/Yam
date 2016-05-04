b7349da8ad1a6f98381bdd7c35500b7c2f10f3fd
lusfold <lusfold@gmail.com>
2016-05-04 14:00:51 +0800

    delete

--- app/src/main/java/com/lusfold/yam/event/BaseEvent.java
+++
@@ -1,7 +0,0 @@
-package com.lusfold.yam.event;
-
-/**
- * Created by lusfold on 5/1/16.
- */
-public class BaseEvent {
-}
--- app/src/main/java/com/lusfold/yam/event/UpdateMailDraftEvent.java
+++
@@ -1,7 +0,0 @@
-package com.lusfold.yam.event;
-
-/**
- * Created by lusfold on 5/2/16.
- */
-public class UpdateMailDraftEvent extends BaseEvent {
-}
